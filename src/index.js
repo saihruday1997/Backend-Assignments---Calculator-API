74dbb8d490df8ba7a7873d1ba19f229612911dd3
saihruday1997 <saihruday.08@gmail.com>
2020-12-14 03:04:31 +0530

    chngs

--- src/index.js
+++ src/index.js
@@ -45,21 +45,67 @@ app.post("/sub" , (req,res) => {
     let num1 = req.body.num1;
     let num2 = req.body.num2;
 
-    res.json({status:"success",message:"the difference of given two numbers",difference: num1-num2});
+    let diff = num1-num2;
+
+    if(num1<under || num2<under || diff<under ){
+        res.json({status: "error", message:"Underflow"});
+    }
+
+    if(num1>over || num2>over || diff>over){
+        res.json({status:"error", message:"Overflow"});
+    }
+
+    if(typeof num1 === "string" || typeof num2 === "string"){
+        res.json({status:"error" , message:"Invalid data types"});
+    }
+
+    res.json({status:"success",message:"the difference of given two numbers",difference: diff});
 });
 
 app.post("/multiply" , (req,res) => {
     let num1 = req.body.num1;
     let num2 = req.body.num2;
 
-    res.json({status:"success",message:"The product of given numbers",result: num1*num2});
+    let prod = num1*num2;
+
+    if(num1<under || num2<under || prod<under ){
+        res.json({status: "error", message:"Underflow"});
+    }
+
+    if(num1>over || num2>over || prod>over){
+        res.json({status:"error", message:"Overflow"});
+    }
+
+    if(typeof num1 === "string" || typeof num2 === "string"){
+        res.json({status:"error" , message:"Invalid data types"});
+    }
+
+    res.json({status:"success",message:"The product of given numbers",result: prod});
 });
 
 app.post("/divide" , (req,res) => {
     let num1 = req.body.num1;
     let num2 = req.body.num2;
 
-    res.json({status:"success",message:"The division of given numbers",result: num1/num2});
+    let div = num1/num2;
+
+    if(num2 === 0){
+        res.json({status: "error", message: "Cannot divide by zero"});
+    }
+
+    if(num1<under || num2<under || div<under ){
+        res.json({status: "error", message:"Underflow"});
+    }
+
+    if(num1>over || num2>over || div>over){
+        res.json({status:"error", message:"Overflow"});
+    }
+
+    if(typeof num1 === "string" || typeof num2 === "string"){
+        res.json({status:"error" , message:"Invalid data types"});
+    }
+
+    res.json({status:"success",message:"The division of given numbers",result: div});
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
